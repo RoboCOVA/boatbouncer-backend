334bea2a7c2c237886236c742b62c6f4b58ef2f0
= <=>
2023-03-19 11:03:16 -0700

    🆕 NEW:  create conversation endpoint

---
+++ src/routes/conversaton.js
@@ -0,0 +1,28 @@
+import { Router } from 'express';
+import {
+  crearteConversationValidator,
+  getConversationValidator,
+} from '../validators/conversation.validator';
+import parseValidationResult from '../validators/errors.parser';
+import {
+  createConversationController,
+  getConversationController,
+} from '../controller/conversations';
+
+const router = Router();
+
+router.post(
+  '/',
+  crearteConversationValidator(),
+  parseValidationResult,
+  createConversationController
+);
+
+router.get(
+  '/',
+  getConversationValidator(),
+  parseValidationResult,
+  getConversationController
+);
+
+export default router;
---
+++ src/routes/message.js
@@ -0,0 +1,28 @@
+import { Router } from 'express';
+import {
+  crearteMessageValidator,
+  getMessagesValidator,
+} from '../validators/message.validators';
+import parseValidationResult from '../validators/errors.parser';
+import {
+  createMessageController,
+  getMessagesController,
+} from '../controller/messages';
+
+const router = Router();
+
+router.post(
+  '/',
+  crearteMessageValidator(),
+  parseValidationResult,
+  createMessageController
+);
+
+router.get(
+  '/',
+  getMessagesValidator(),
+  parseValidationResult,
+  getMessagesController
+);
+
+export default router;
