fe274f285301545a8e26d453b544aba85ed3c04f
leol <leul@addissoftware.com>
2023-05-13 09:10:40 -0700

    render hosting deployment ready

--- src/config/express.js
+++ src/config/express.js
@@ -8,8 +8,6 @@ import * as environments from './environments';
 import APIError from '../errors/APIError';
 import routes from './routes';
 import { stripeWebHookController } from '../controller/webhook';
-import adminRoute from './admin';
-import { adminJs } from './admin/config';
 
 const app = express();
 
@@ -27,7 +25,6 @@ if (environments.nodeEnv !== 'test') {
 
 // AdminBro/Js
 app.use(cors());
-app.use(adminJs.options.rootPath, adminRoute);
 // Secure middlewares
 app.use(helmet());
 
