0950913aecd1aeaca32caa8d5d1b5859b74ae1b1
Habtamu Bogale <abubogale342@gmail.com>
2024-05-28 21:53:16 +0300

    added image upload limiter

--- src/config/express.js
+++ src/config/express.js
@@ -37,7 +37,11 @@ app.use(
   express.raw({ type: 'application/json' }),
   stripeWebHookController
 );
-app.use(express.json());
+app.use(
+  express.json({
+    limit: '50mb',
+  })
+);
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/boatbouncer', routes);
