37f1a510d0de7516ef3791b6c1231aa39ee8c24b
5237-mests <msfnmulgeta@gmail.com>
2024-06-09 00:57:41 +0300

    Create new Admin functionality done.

--- src/config/admin/resources/adminstrators.js
+++ src/config/admin/resources/adminstrators.js
@@ -1,49 +1,18 @@
-import bcrypt from 'bcrypt';
 import Adminstrators from '../../../models/Adminstrators';
 
 export const AdminResource = {
   resource: Adminstrators,
   options: {
     properties: {
-      newPassword: {
-        type: 'custom',
-        label: 'New Password',
-        isVisible: {
-          show: false,
-          edit: true,
-          list: false,
-          filter: false,
-        },
-      },
+      _id: { isVisible: false },
       createdAt: { isVisible: false },
       updatedAt: { isVisible: false },
       password: {
-        type: 'password',
-        isVisible: false,
-      },
-    },
-    actions: {
-      new: {
-        before: async (request) => {
-          if (request?.payload?.password) {
-            request.payload = {
-              ...request.payload,
-              password: await bcrypt.hash(request.payload.password, 10),
-            };
-          }
-          return request;
-        },
-      },
-      edit: {
-        before: async (request) => {
-          if (request?.payload?.newPassword) {
-            request.payload = {
-              ...request.payload,
-              password: await bcrypt.hash(request.payload.newPassword, 10),
-            };
-            delete request.payload.newPassword;
-          }
-          return request;
+        isVisible: {
+          list: false,
+          edit: true,
+          show: true,
+          filter: false,
         },
       },
     },
