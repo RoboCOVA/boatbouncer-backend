cee05fafebe3efc9bf21f9dfdf876b50fabbdf49
5237-mests <msfnmulgeta@gmail.com>
2024-06-09 00:59:19 +0300

    Small modification done.

--- src/config/admin/resources/boats.js
+++ src/config/admin/resources/boats.js
@@ -7,6 +7,118 @@ export const BoatsResource = {
       _id: {
         isVisible: false,
       },
+      amenities: {
+        isVisible: {
+          list: false,
+          edit: true,
+          show: true,
+          filter: false,
+        },
+      },
+      'location.city': {
+        isVisible: {
+          list: false,
+          edit: true,
+          show: true,
+          filter: false,
+        },
+      },
+      'location.state': {
+        isVisible: {
+          list: false,
+          edit: true,
+          show: true,
+          filter: false,
+        },
+      },
+      'location.zipCode': {
+        isVisible: {
+          list: false,
+          edit: true,
+          show: true,
+          filter: false,
+        },
+      },
+      imageUrls: {
+        isVisible: {
+          list: false,
+          edit: true,
+          show: true,
+          filter: false,
+        },
+      },
+      'latLng.type': {
+        isVisible: {
+          list: false,
+          edit: true,
+          show: true,
+          filter: false,
+        },
+      },
+      'latLng.coordinates': {
+        isVisible: {
+          list: false,
+          edit: true,
+          show: true,
+          filter: false,
+        },
+      },
+      pricing: {
+        isVisible: {
+          list: false,
+          edit: true,
+          show: true,
+          filter: false,
+        },
+      },
+      category: {
+        isVisible: {
+          list: false,
+          edit: true,
+          show: true,
+          filter: false,
+        },
+      },
+      subCategory: {
+        isVisible: {
+          list: false,
+          edit: true,
+          show: true,
+          filter: false,
+        },
+      },
+      currency: {
+        isVisible: {
+          list: false,
+          edit: true,
+          show: true,
+          filter: false,
+        },
+      },
+      features: {
+        isVisible: {
+          list: false,
+          edit: true,
+          show: true,
+          filter: false,
+        },
+      },
+      securityAllowance: {
+        isVisible: {
+          list: false,
+          edit: true,
+          show: true,
+          filter: false,
+        },
+      },
+      captained: {
+        isVisible: {
+          list: false,
+          edit: true,
+          show: true,
+          filter: false,
+        },
+      },
       createdAt: {
         isVisible: {
           list: false,
