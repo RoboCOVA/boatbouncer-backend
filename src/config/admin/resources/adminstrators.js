8b3c01b8c12d8549691dc7666690244dd3b03df1
5237-mests <msfnmulgeta@gmail.com>
2024-06-10 14:57:08 +0300

    Bug fixed from admin resourse.

--- src/config/admin/resources/adminstrators.js
+++ src/config/admin/resources/adminstrators.js
@@ -1,3 +1,4 @@
+import bcrypt from 'bcrypt';
 import Adminstrators from '../../../models/Adminstrators';
 
 export const AdminResource = {
@@ -5,14 +6,41 @@ export const AdminResource = {
   options: {
     properties: {
       _id: { isVisible: false },
+      password: {
+        type: 'custom',
+        label: 'Password',
+        isVisible: {
+          show: false,
+          edit: true,
+          list: false,
+          filter: false,
+        },
+      },
       createdAt: { isVisible: false },
       updatedAt: { isVisible: false },
-      password: {
-        isVisible: {
-          list: false,
-          edit: true,
-          show: true,
-          filter: false,
+    },
+    actions: {
+      new: {
+        before: async (request) => {
+          if (request?.payload?.password) {
+            request.payload = {
+              ...request.payload,
+              password: await bcrypt.hash(request.payload.password, 10),
+            };
+          }
+          return request;
+        },
+      },
+      edit: {
+        before: async (request) => {
+          if (request?.payload?.password) {
+            request.payload = {
+              ...request.payload,
+              password: await bcrypt.hash(request.payload.password, 10),
+            };
+            delete request.payload.password;
+          }
+          return request;
         },
       },
     },
