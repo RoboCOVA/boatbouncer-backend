7d400392e6484e66983794c3e8888a2b9bb29857
Habtamu Bogale <abubogale342@gmail.com>
2024-08-15 19:32:30 +0300

    disabled captain price

--- src/models/Offers/schema.js
+++ src/models/Offers/schema.js
@@ -7,7 +7,7 @@ const offerSchema = new mongoose.Schema(
     bookId: { type: Types.ObjectId, ref: modelNames.BOOKINGS, required: true },
     boatPrice: { type: Number, required: true },
     captainPrice: { type: Number, min: 0 },
-    paymentServiceFee: { type: Number, required: true },
+    // paymentServiceFee: { type: Number, required: true },
     localTax: { type: Number, required: true },
     status: { type: String, enum: offerStatusEnum },
     departureDate: { type: Date, required: true },
--- src/validators/offer.validators.js
+++ src/validators/offer.validators.js
@@ -5,9 +5,9 @@ export const createOffervalidator = () => [
   body('bookId').isMongoId().withMessage('Valid Book id is required'),
   body('boatPrice').isNumeric().withMessage('Boat Price is required'),
   body('captainPrice').isNumeric().optional(),
-  body('paymentServiceFee')
-    .isNumeric()
-    .withMessage('Payment Service Fee is required'),
+  // body('paymentServiceFee')
+  //   .isNumeric()
+  //   .withMessage('Payment Service Fee is required'),
   body('localTax').isNumeric().withMessage('Local Tax is required'),
   body('returnDate')
     .custom(customDateValidator)
