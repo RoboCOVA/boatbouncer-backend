cef3c91e6fb6ac9cb76e21787c05dd16d060ee65
Habtamu Bogale <abubogale342@gmail.com>
2025-05-28 11:21:33 +0300

    fix: increase_size_limit

--- src/config/express.js
+++ src/config/express.js
@@ -42,10 +42,10 @@ app.use(
 );
 app.use(
   express.json({
-    limit: '50mb',
+    limit: '100mb',
   })
 );
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 
 app.use('/api/boatbouncer', routes);
 
